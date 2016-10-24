849829acbee88de08e2c4efd42580553d6561293
Dan Puttick <dputtick@users.noreply.github.com>
2016-10-24 15:19:18 -0400

    Added crowsfoot theme as a submodule

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,27 +2,24 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# Basic config
 AUTHOR = 'Daniel Puttick'
 SITENAME = 'Daniel Puttick'
 SITEURL = 'https://danielputtick.com'
 THEME = 'simple'
 PATH = 'content'
-
 TIMEZONE = 'America/New_York'
-
 DEFAULT_LANG = 'en'
 DEFAULT_CATEGORY = 'articles'
-
 ARTICLE_PATHS = ['/articles']
-
 TYPOGRIFY = True
+DEFAULT_PAGINATION = False
+DELETE_OUTPUT_DIRECTORY = False
 
 # Social widget
 SOCIAL = (('Github', 'http://www.github.com/dputtick'),
           ('Twitter', 'http://www.twitter.com/dputtick'),)
 
-DEFAULT_PAGINATION = False
-DELETE_OUTPUT_DIRECTORY = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
