887c99cfa4c06a66f0ae4b22b9d67ea13a1c642d
Dan Puttick <dputtick@users.noreply.github.com>
2017-03-18 20:09:23 -0400

    Update settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,15 +3,16 @@
 from __future__ import unicode_literals
 
 # Basic config
-AUTHOR = 'Daniel Puttick'
+AUTHOR = 'Dan Puttick'
 SITENAME = 'Daniel Puttick'
-SITEURL = 'https://danielputtick.com'
+SITEURL = 'http://danielputtick.com'
 THEME = 'themes/crowsfoot'
 PATH = 'content/'
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG = 'en'
 DEFAULT_CATEGORY = 'misc'
-#ARTICLE_PATHS = ['blog']
+PAGE_PATHS = ['pages']
+ARTICLE_PATHS = ['writing']
 DIRECT_TEMPLATES = ['index']
 TYPOGRIFY = True
 DEFAULT_PAGINATION = False
@@ -25,8 +26,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Social widget
-SOCIAL = (('Github', 'http://www.github.com/dputtick'),
-          ('Twitter', 'http://www.twitter.com/dputtick'),)
+SOCIAL = (('Github', 'https://www.github.com/dputtick'),
+          ('Twitter', 'https://www.twitter.com/dputtick'),)
 
 # Crowsfoot specific settings
 SHOW_ARTICLE_AUTHOR = False
@@ -37,4 +38,4 @@ TWITTER_ADDRESS = 'https://www.twitter.com/dputtick'
 MENUITEMS = []
 
 # Uncomment following lines if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
