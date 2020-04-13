67f83a32130611fb58945ccb335a4dd0b2ad7644
Dan Puttick <dputtick@users.noreply.github.com>
2020-04-13 18:35:17 -0400

    Add favicon

--- publishconf.py
+++ publishconf.py
@@ -9,7 +9,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # Reconfigure for production
-SITEURL = 'https://danielputtick.com'
+SITEURL = 'https://www.danielputtick.com'
 RELATIVE_URLS = False
 
 # Feed settings
